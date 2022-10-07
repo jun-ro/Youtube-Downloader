3e7b23d4ec474e28f67af2440be0c735baa2624c
Lozarth <46830521+Lozarth@users.noreply.github.com>
2022-10-07 18:21:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const fs = require('fs')
 const youtubedl = require('ytdl-core')
-const config = require('./config.json')
+const readline = require('readline')
 
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
+  var result = ''
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+  var charactersLength = characters.length
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength))
+  }
+  return result
 }
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 
-
-
-youtubedl(config.youtubeURL).pipe(fs.createWriteStream('jun-' + makeid(5) + '.mp4'));
+rl.question('Enter the video URL: ', async (url) => {
+  console.log('Downloading video...')
+  await youtubedl(url).pipe(fs.createWriteStream(`video-${makeid(4)}.mp4`))
+  console.log('Video downloaded!')
+  process.exit()
+})
